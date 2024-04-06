b0c7c365e60b75a91c0638d8413ab763b8003af3
Jan T. Sott <git@idleberg.com>
2024-04-06 16:23:16 +0200

    downgrade dropboxignore

--- Formula/dropboxignore.rb
+++ Formula/dropboxignore.rb
@@ -1,8 +1,8 @@
 class Dropboxignore < Formula
   desc "Command-line tool for ignoring files in Dropbox"
   homepage "https://dropboxignore.simakis.me/"
-  url "https://github.com/sp1thas/dropboxignore/archive/refs/tags/v1.2.3.tar.gz"
-  sha256 "4ef5981c24fe6f5f4c9d4e955f396d48e56ef82b5b0c6fdf8cecb54572e17cf5"
+  url "https://github.com/sp1thas/dropboxignore/archive/refs/tags/v1.1.0.tar.gz"
+  sha256 "033fa1ca2593489772ea3af97a665f5de4a1751fce6bb8b246322e8835cb402d"
   license "GPL-3.0-only"
 
   bottle do
