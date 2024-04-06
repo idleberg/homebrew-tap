95ee76ee4e3db6e1c13dab4e3648075c12fcb7d0
Jan T. Sott <git@idleberg.com>
2024-04-06 18:25:38 +0200

    fix path

--- Formula/dbxcli.rb
+++ Formula/dbxcli.rb
@@ -15,7 +15,7 @@ class Dbxcli < Formula
   end
 
   def install
-    bin.install "dbxcli-#{OS.mac? ? "darwin" : "linux"}-#{Hardware::CPU.arm? ? "arm" : "amd64"} " => "dbxcli"
+    bin.install "dbxcli-#{OS.mac? ? "darwin" : "linux"}-#{Hardware::CPU.arm? ? "arm" : "amd64"}" => "dbxcli"
   end
 
   test do
