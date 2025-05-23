98d6e8f6a02e34626cd39696413d018bb115e2bf
Jan T. Sott <git@idleberg.com>
2025-05-23 23:32:16 +0200

    fix typo

--- Formula/dbxcli.rb
+++ Formula/dbxcli.rb
@@ -4,7 +4,7 @@ class Dbxcli < Formula
   url "https://github.com/dropbox/dbxcli/releases/download/v3.0.0/dbxcli-darwin-amd64"
   sha256 "1149a2aa6a89829c6d540d04cc1db8cf5bb27e3d8b0ec6b32d830a6818bd7573"
   license "MIT"
-  head "https://github.com/dropbox/dbxcl.git", branch: "master"
+  head "https://github.com/dropbox/dbxcli.git", branch: "master"
 
   def install
     bin.install "dbxcli-darwin-amd64" => "dbxcli"
